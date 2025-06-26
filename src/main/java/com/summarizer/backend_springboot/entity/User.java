941ccc6a5c1dad937a690e65bd9d75433c49e579
kiwon <kkw-11@naver.com>
2025-06-26 20:55:04 +0900

    feat:User Entity

---
+++ src/main/java/com/summarizer/backend_springboot/entity/User.java
@@ -0,0 +1,32 @@
+package com.summarizer.backend_springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 120)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Boolean isGuest = false;
+
+    private LocalDateTime lastLoginAt;
+}
+
