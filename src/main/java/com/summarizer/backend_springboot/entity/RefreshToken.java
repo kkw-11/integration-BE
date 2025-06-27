729d59b87ba3f9b4811fe9fc09844e8203d50441
kiwon <kkw-11@naver.com>
2025-06-27 18:59:29 +0900

    feat:RefreshToken Entity

---
+++ src/main/java/com/summarizer/backend_springboot/entity/RefreshToken.java
@@ -0,0 +1,29 @@
+package com.summarizer.backend_springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "refresh_token")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshToken extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Column(nullable = false, unique = true, length = 512)
+    private String token;
+
+    private LocalDateTime expiredAt;
+}
+
